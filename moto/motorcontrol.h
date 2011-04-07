c275a0062c0dc1d93f8dc28f6cfb86090fdbf15c
villisika <guseribja@student.gu.se>
2011-04-07 15:15:14 +0200

    hhmmm

--- moto/motorcontrol.h
+++ moto/motorcontrol.h
@@ -1,4 +1,4 @@
-#define LEFT_MOTOR 2
+#define LEFT_MOTOR 6
 #define RIGHT_MOTOR 3
 #define FRONT_MOTOR 4
 #define REAR_MOTOR 5
@@ -6,6 +6,6 @@
 void startMotors();
 void stopMotors();
 unsigned char increaseMotorPulse(unsigned char motor, unsigned char pulse);
-unsigned char increaseMotorPulsePanic(unsigned char motor, unsigned char pulse);
+unsigned char increaseMotorPulseX2(unsigned char motor, unsigned char pulse);
 unsigned char decreaseMotorPulse(unsigned char motor, unsigned char pulse);
-unsigned char decreaseMotorPulsePanic(unsigned char motor, unsigned char pulse);
+unsigned char decreaseMotorPulseX2(unsigned char motor, unsigned char pulse);
