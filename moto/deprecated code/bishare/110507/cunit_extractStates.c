6962ad3aa7b873ba8a296e1785d1aa8777411f20
Bishare Sufi <bishare@ubuntu.(none)>
2011-04-14 11:49:43 -0700

    added deprecated code

--- moto/deprecated code/bishare/110507/cunit_extractStates.c
+++ moto/deprecated code/bishare/110507/cunit_extractStates.c
@@ -1,9 +1,9 @@
 /*@uthor Bishare_sufi*/
 #include <stdio.h>
 #include "CUnit/CUnit.h"
-#include "../src/extract_states.h"
+#include "extract_states.h"
 
-/* run cunit with gcc cunit_extractStates.c ../src/exract_states.c -DPC -o 
+/* run cunit with gcc cunit_extractStates.c exract_states.c -o 
 cunit_extractStates  -lcunit */   /* ./cunit_extractStates */
 /* when you compile this comment the main in extract_states.c */
 
@@ -11,8 +11,10 @@ void test_motors(void){
 
 /*asserts for generate function */
 /* I just wanted to check if it was possible to test my own code */
+/*asserts for 
+  generate function I just check if the code is reacting as expected*/
 
-CU_ASSERT(generate_value_Con_motor(FORWARD) == FORWARD); /*asserts for generate function I just check if the code is reacting as expected*/
+CU_ASSERT(generate_value_Con_motor(FORWARD) == FORWARD); 
 CU_ASSERT(generate_value_Con_motor(BACKWARD) == BACKWARD);
 CU_ASSERT(generate_value_Con_motor(TURN_LEFT) == TURN_LEFT);
 CU_ASSERT(generate_value_Con_motor(TURN_RIGHT) == TURN_RIGHT);
@@ -21,12 +23,18 @@ CU_ASSERT(generate_value_Con_motor(TURN_RIGHT) == TURN_RIGHT);
 
 void test_motors1(void){
 
-CU_ASSERT(generate_value_Pan_motor(FORWARD) == FORWARD); /*asserts for generate function I just check if the code is reacting as expected*/
+ /*asserts for 
+   generate function I just check if the code is reacting as expected*/
+
+CU_ASSERT(generate_value_Pan_motor(FORWARD) == FORWARD);
 CU_ASSERT(generate_value_Pan_motor(BACKWARD) == BACKWARD);
 CU_ASSERT(generate_value_Pan_motor(TURN_LEFT) == TURN_LEFT);
 CU_ASSERT(generate_value_Pan_motor(TURN_RIGHT) == TURN_RIGHT);
 
-CU_ASSERT(generate_value_Pan_motor(INCREASE_MOTOR | LEFT_MOTOR) == INCREASE_MOTOR | LEFT_MOTOR); /*asserts for generate function I just check if the code is reacting as expected*/ 
+/*asserts for 
+  generate function I just check if the code is reacting as expected*/ 
+
+CU_ASSERT(generate_value_Pan_motor(INCREASE_MOTOR | LEFT_MOTOR) == INCREASE_MOTOR | LEFT_MOTOR); 
 CU_ASSERT(generate_value_Pan_motor(INCREASE_MOTOR | RIGHT_MOTOR) == INCREASE_MOTOR | RIGHT_MOTOR);
 CU_ASSERT(generate_value_Pan_motor(INCREASE_MOTOR | LEFT_MOTOR | RIGHT_MOTOR) == INCREASE_MOTOR | LEFT_MOTOR | RIGHT_MOTOR);
 
@@ -34,7 +42,10 @@ CU_ASSERT(generate_value_Pan_motor(INCREASE_MOTOR | FRONT_MOTOR) == INCREASE_MOT
 CU_ASSERT(generate_value_Pan_motor(INCREASE_MOTOR | REAR_MOTOR) == INCREASE_MOTOR | REAR_MOTOR);
 CU_ASSERT(generate_value_Pan_motor(INCREASE_MOTOR | FRONT_MOTOR | REAR_MOTOR) == INCREASE_MOTOR | FRONT_MOTOR | REAR_MOTOR);
 
-CU_ASSERT(generate_value_Pan_motor(DECREASE_MOTOR | LEFT_MOTOR) == DECREASE_MOTOR | LEFT_MOTOR); /*asserts for generate function I just check if the code is reacting as expected*/ 
+/*asserts for 
+generate function I just check if the code is reacting as expected*/ 
+
+CU_ASSERT(generate_value_Pan_motor(DECREASE_MOTOR | LEFT_MOTOR) == DECREASE_MOTOR | LEFT_MOTOR); 
 CU_ASSERT(generate_value_Pan_motor(DECREASE_MOTOR | RIGHT_MOTOR) == DECREASE_MOTOR | RIGHT_MOTOR);
 CU_ASSERT(generate_value_Pan_motor(DECREASE_MOTOR | LEFT_MOTOR | RIGHT_MOTOR) == DECREASE_MOTOR | LEFT_MOTOR | RIGHT_MOTOR);
 
@@ -46,7 +57,10 @@ CU_ASSERT(generate_value_Pan_motor(DECREASE_MOTOR | FRONT_MOTOR | REAR_MOTOR) ==
 
 void test_motors2(void){
 
-CU_ASSERT(generate_value_Inc_motor(INCREASE_MOTOR | LEFT_MOTOR) == INCREASE_MOTOR | LEFT_MOTOR);/*asserts for generate function I just check if the code is reacting as expected*/ 
+/*asserts for 
+  generate function I just check if the code is reacting as expected*/
+ 
+CU_ASSERT(generate_value_Inc_motor(INCREASE_MOTOR | LEFT_MOTOR) == INCREASE_MOTOR | LEFT_MOTOR);
 CU_ASSERT(generate_value_Inc_motor(INCREASE_MOTOR | RIGHT_MOTOR) == INCREASE_MOTOR | RIGHT_MOTOR);
 CU_ASSERT(generate_value_Inc_motor(INCREASE_MOTOR | LEFT_MOTOR | RIGHT_MOTOR) == INCREASE_MOTOR | LEFT_MOTOR | RIGHT_MOTOR);
 
@@ -58,7 +72,10 @@ CU_ASSERT(generate_value_Inc_motor(INCREASE_MOTOR | FRONT_MOTOR | REAR_MOTOR) ==
 
 void test_motors3(void){
 
-CU_ASSERT(generate_value_Dec_motor(DECREASE_MOTOR | LEFT_MOTOR) == DECREASE_MOTOR | LEFT_MOTOR);/*asserts for generate function I just check if the code is reacting as expected*/ 
+/*asserts for 
+  generate function I just check if the code is reacting as expected*/ 
+
+CU_ASSERT(generate_value_Dec_motor(DECREASE_MOTOR | LEFT_MOTOR) == DECREASE_MOTOR | LEFT_MOTOR);
 CU_ASSERT(generate_value_Dec_motor(DECREASE_MOTOR | RIGHT_MOTOR) == DECREASE_MOTOR | RIGHT_MOTOR);
 CU_ASSERT(generate_value_Dec_motor(DECREASE_MOTOR | LEFT_MOTOR | RIGHT_MOTOR) == DECREASE_MOTOR | LEFT_MOTOR | RIGHT_MOTOR);
 
