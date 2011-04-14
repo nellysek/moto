55c8877ad8ed3622cab78b8f01ed7577c330f749
Bishare Sufi <bishare@ubuntu.(none)>
2011-04-14 12:26:59 -0700

    added deprecated code

--- moto/deprecated code/bishare/110507/extract_states.h
+++ moto/deprecated code/bishare/110507/extract_states.h
@@ -3,8 +3,7 @@
 #define TURN_RIGHT 0xEC
 #define TURN_LEFT 0xCE
 #define BACKWARD 0xC3
-
-/*#define PANIC_STATE 0x80
+#define PANIC_STATE 0x80
 #define START_STATE 0x40
 #define INCREASE_MOTOR 0x20
 #define DECREASE_MOTOR 0x10
@@ -12,7 +11,7 @@
 #define LEFT_MOTOR 0x4
 #define FRONT_MOTOR 0x2
 #define REAR_MOTOR 0x1
-#define STOP_STATE 0x0*/
+#define STOP_STATE 0x0
 
 unsigned char generate_value_Inc_motor(unsigned char motor);
 unsigned char generate_value_Dec_motor(unsigned char motor);
