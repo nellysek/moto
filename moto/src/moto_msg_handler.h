935d6df0704815cd1d462347ec0dcda4e448b5fd
Kristofer Hansson <k.hansson.aspman@gmail.com>
2011-04-19 15:09:39 +0200

    adding stuff back

---
+++ moto/src/moto_msg_handler.h
@@ -0,0 +1,24 @@
+/**
+ * Module:       parser.h
+ * Author(s):    Kristofer Hansson Aspman
+ *
+ * Description:  The typedef of the bitfield struct and
+ *               all the prototypes for the functions used
+ *               by tools.c, motor_control.c and parser.c.
+ *
+ */
+
+#ifdef ARDUINO
+   #include "WProgram.h"
+#elif defined PC
+   #include <stdio.h>
+#endif
+
+//Parser functions
+int examineID(msg_pointer mp);
+void controlMotors(msg_pointer mp);
+void specialMotorCommand(msg_pointer mp);
+msg scanHexMsgSTDIN(void);
+#ifdef ARDUINO
+unsigned char serReadUnsignedChar(void);
+#endif
