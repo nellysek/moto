3205da7b9323900cef43a4f0571212293b1e74e6
STORmogulen <gusbermaal@student.gu.se>
2011-05-17 22:44:50 +0200

    Updates after code review

--- moto/src/moto_interface.c
+++ moto/src/moto_interface.c
@@ -1,21 +1,24 @@
-/*!@author Kristofer Hansson Aspman, Björn Eriksson, Magnus Bergqvist
+/*!
+ * @author Kristofer Hansson Aspman, Björn Eriksson, Magnus Bergqvist
  * @file moto_interface.c
- * @version v0.05
+ * @version v0.06
  * @date 2011-05-16
- * @history 2011-05-16 - Implemented no-op instructions
-                        (Kristofer)
-            2011-05-11 - Worked with ifdefs
-                        (Magnus, Björn)
-            2011-05-10 - Added support for struct containing 8 messages
-                        (Magnus, Björn, Kristofer)
+ * @history 2011-05-17 - Updated after code review \n
+                        (Magnus) \n
+            2011-05-16 - Implemented no-op instructions \n
+                        (Kristofer) \n
+            2011-05-11 - Worked with ifdefs \n
+                        (Magnus, Björn) \n
+            2011-05-10 - Added support for struct containing 8 messages \n
+                        (Magnus, Björn, Kristofer) \n
             2011-04-26 - Major cleanup and restructuring of the code\n
-                        (Magnus, Björn, Kristofer)
-            2011-04-10 - Created first version
-                        (Magnus, Björn, Kristofer)
+                        (Magnus, Björn, Kristofer) \n
+            2011-04-10 - Created first version \n
+                        (Magnus, Björn, Kristofer) \n
 
  * @brief Contains the implementations of moto_init and moto_run.\n
-          Global variables set are: mp (a pointer to the hexadecimal message
- *        read from the protocol).
+          Global variables set are: mp (a pointer to the hexadecimal \n
+          message read from the protocol).
  */
 
 #include <stdint.h>
@@ -38,8 +41,6 @@
 msg_pointer mp;
 
 #ifdef ARDUINO
-/* Pins for testing */
-int ledPin = 13;
 /* Servo "objects" definitions for the ESCs */
 Servo escRight;
 Servo escLeft;
@@ -47,15 +48,12 @@ Servo escFront;
 Servo escRear;
 #endif
 
-/*! @author Kristofer Hansson Aspman, Björn Eriksson, Magnus Bergqvist
- * @brief The init function requested by the CFG. It is
-          called when the drone boots up.
- * @version v0.04
- * @date 2011-05-01
- * @param none
- * @return int (0 if correctly carried out)
+/* 
+ * author(s): Kristofer Hansson Aspman, Björn Eriksson, Magnus Bergqvist
+ * The moto_init function requested by the CFG. It is called when the 
+ * drone boots up.
  */
-int moto_init(void){
+uint8_t moto_init(void){
     PRINT_STRING("Inside moto_init");
     PRINT_NEW_LINE;
 #ifdef ARDUINO
@@ -67,40 +65,35 @@ int moto_init(void){
     escLeft.writeMicroseconds(STOP_PULSE);
     escFront.writeMicroseconds(STOP_PULSE);
     escRear.writeMicroseconds(STOP_PULSE);
-    pinMode(ledPin, OUTPUT);
 #endif
   moto_cyclesSinceLastMsg = 0;
   return 0;
 }
 
-/*! @author Kristofer Hansson Aspman
- * @brief The run function requested by the CFG. It is run
-          each scheduled cycle.
- * @version v0.04
- * @date 2011-05-16
- * @param none
- * @return int (0 if correctly carried out)
+/*
+ * author(s): Kristofer Hansson Aspman
+ * The moto_run function requested by the CFG. It is run each scheduled cycle.
  */
-int moto_run(void){
+uint8_t moto_run(void){
     moto_cyclesSinceLastMsg++;
     PRINT_STRING("Number of cycles without a message: ");
     PRINT_DEC(moto_cyclesSinceLastMsg);
     PRINT_NEW_LINE;
 
-/* Initiates no-op command if no message has been written to the protocol
- * in 5000 cycles. No-op command is currently set to HOVER but can be changed
- * whatever command preferred.
- */
-    if (moto_cyclesSinceLastMsg > 5000)
-    {
+    /* 
+     * Initiates no-op command if no message has been written to the protocol
+     * in 5000 cycles. No-op command is currently set to HOVER but 
+     * can be changed whatever command preferred.
+     */
+    if (moto_cyclesSinceLastMsg > 5000){
         PRINT_STRING("No new message in protocol for too long!");
         PRINT_NEW_LINE;
         PRINT_STRING("Initiating no-op command!");
         PRINT_NEW_LINE;
 
-/* Set the uint8_t to HOVER and then casting it to a msg before sending
- * it to examineID() to be parsed.
- */
+        /* Set the uint8_t to HOVER and then casting it to a msg 
+         * before sending it to examineID() to be parsed.
+         */
         uint8_t no_op_hex = HOVER;
         msg no_op_msg = INT_TO_BITFIELD(&no_op_hex);
         mp = &no_op_msg;
@@ -109,24 +102,31 @@ int moto_run(void){
         return 0;
     }
 
-/*
- *  when moto_recvMsg() is used
- *  ----------------------------------------------
- *   mp = moto_recvMsg(); 
- *  ----------------------------------------------
- */
+    /*
+     *  when moto_recvMsg() is used
+     *  ----------------------------------------------
+     *   mp = moto_recvMsg(); 
+     *  ----------------------------------------------
+     */
 
-/*
- * if moto_recvMsg2() is used
- * 
- * for loop and "uint8_t i" SHALL be removed
- */
+    /*
+     * if moto_recvMsg() is used
+     * 
+     * for loop statements and "uint8_t i" SHALL be removed
+     */
 
-/* ---------------this one------------------------ */
+    /* ---------------this one------------------------ */
+    
+    /* 
+     * Creates a msgStructPtr and fetches data from mot_recvMsg2, it then
+     * picks out the poiter to the first message in the struct. The rest
+     * of the messages will be iterated over in the following for-loop
+     */
     msgStructPtr mpStruct;
     mpStruct = moto_recvMsg2();
     mp = (msg_pointer)&(mpStruct->msg1);
-/* ----------------------------------------------- */
+    
+    /* ----------------------------------------------- */
 
 
 
@@ -143,25 +143,25 @@ int moto_run(void){
             return 0;
         }
 
-/*Resets the counter that counts how many cycles has run since last message*/
-/*was written to protocol.*/
+        /*
+         * Resets the counter that counts how many cycles has run since
+         * last message was written to protocol.
+         */
         moto_cyclesSinceLastMsg = 0;
-        if(BITFIELD_TO_CHAR(mp) < 0x0 || BITFIELD_TO_CHAR(mp) > 0xff)
-        {
+        
+        if(BITFIELD_TO_CHAR(mp) < 0x0 || BITFIELD_TO_CHAR(mp) > 0xff){
             PRINT_STRING("Message out of bounds");
             PRINT_NEW_LINE;
             return 0;
         }
 
-        if(BITFIELD_TO_CHAR(mp) == 0xf)
-        {
+        if(BITFIELD_TO_CHAR(mp) == 0xf){
             PRINT_STRING("No new message in scanhexmsg");
             PRINT_NEW_LINE;
             return 0;
         }
 
-        if(BITFIELD_TO_CHAR(mp) == 0xB)
-        {
+        if(BITFIELD_TO_CHAR(mp) == 0xB){
             PRINT_STRING("Struct does not contain more messages!");
             PRINT_NEW_LINE;
             return 0;
@@ -171,13 +171,9 @@ int moto_run(void){
 
         printMsg(mp);
         printMotorStatus();
-/*
- *
- *  mp++ for moto_recvMsg2() 
- *  used when using the struct of messages
- * 
- */
+        
+        /* pointer increment to get the next message in struct */
         mp++;
-        } /* ends the for loop */
+        }
     return 0;
 }
